0694ae374d26211fed799795e58a0a0e1ff2dfeb
이수용 <suyong5713@gmail.com>
2022-10-22 19:22:58 +0900

    [Baekjoon-17387] 새로운 게임2

--- Baekjoon/suyong/17387_새로운 게임2.py
+++ Baekjoon/suyong/17387_새로운 게임2.py
@@ -12,10 +12,13 @@ for idx, horse in enumerate(horse_info):
 turn = 0
 flag = False
 while True:
+  turn += 1
+  #턴 1000 넘어가면 종료
   if turn >= 1000:
     print(-1)
     break
   for idx, horse in enumerate(horse_info):
+    #이동하는 말 위에 얹어진 말 리스트
     top = []
     row, col, direction = horse
     drow, dcol = dir[direction]
@@ -26,25 +29,31 @@ while True:
         direction += 1
       elif direction in [2,4]:
         direction -= 1
+      #반전된 방향을 반영
       horse_info[idx][2] = direction
       nrow, ncol = row + (drow * -1), col + (dcol * -1)
       #방향 바꿨는데도 파란색 or 범위 밖이면 이동 안함
       if not(0 <= nrow < N and 0 <= ncol < N) or board[nrow][ncol] == 2:
         continue
+    #현재 말이 몇번째에 쌓여있는지 저장
     index = board_horse[row][col].index(idx)
+    #현재 말 포함 위에있는 말 저장
     top.extend(board_horse[row][col][index:])
     if board[nrow][ncol] == 1:
-      #빨간색이면 말 위에 있는 말들 순서 반전
+      #빨간색이면 말 얹어진 말들 순서 반전
       top = list(reversed(top))
+    #이동할 칸 위에 쌓아줌
     board_horse[nrow][ncol].extend(top)
+    #원래 있던 칸에서 이동한 말들 제거
     board_horse[row][col] = board_horse[row][col][:index]
+    #얹어져서 이동된 말들 위치 정보 갱신
     for horse in top:
       horse_info[horse][0] = nrow
       horse_info[horse][1] = ncol
+    #칸에 말이 4개 이상 쌓이면 끝
     if len(board_horse[nrow][ncol]) >= 4:
       flag = True 
       break
-  turn += 1
   if flag:
     print(turn)
     break
